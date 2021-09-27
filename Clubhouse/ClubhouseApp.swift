ad03123b855d86cef9bcbf042c9b1b403101656a
Devesh Tyagi <tyagi77@outlook.com>
2021-09-27 15:14:13 +0530

    Initial Commit

---
+++ Clubhouse/ClubhouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubhouseApp.swift
+//  Clubhouse
+//
+//  Created by Devesh Tyagi on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ClubhouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
